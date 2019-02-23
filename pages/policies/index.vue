760dc4a5bf49a8613ccb024c8d2e3b06714c49a7
Kevin Lewis <kvn@lws.io>
2019-02-23 18:05:10 +0000

    /policies

--- pages/policies/index.vue
+++ pages/policies/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class='w'>
+  <div class='w content'>
     <h1 id="title">Our policies</h1>
+    <p>There are a number of policies created by the BASH Festival team to ensure everyone is safe and happy throughout the event.</p>
+    <p>They're listed below. If you have any questions about them, please <n-link to='/contact'>contact us</n-link>.</p>
+    <ul>
+      <li><n-link to='/policies/conduct'>Code of Conduct</n-link></li>
+      <li><n-link to='/policies/health-safety'>Health & Safety Policy</n-link></li>
+      <li><n-link to='/policies/conduct'>Safeguarding Policy</n-link></li>
+    </ul>
   </div>
 </template>
