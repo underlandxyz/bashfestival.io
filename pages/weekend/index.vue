449352ec02211b9e017a7c6479e89f3302476372
Kevin Lewis <kvn@lws.io>
2019-03-27 13:12:08 +0000

    weekend page

--- pages/weekend/index.vue
+++ pages/weekend/index.vue
@@ -4,8 +4,8 @@
     <div class='w content'>
       <h1 id="title">Weekend Finale</h1>
       <p>The weekend finale of BASH Festival of Code is a chance for you to showcase your projects, learn new skills through our multi-stream conference, and make like-minded friends from across the UK.</p>
-      <p>It starts with a science fair and then transitions into a conference until the evening whwn we will have multiple chill activities to choose from - movies, games, and more. If you have specific activities you'd like to see - <a href="https://twitter.com/BASHFestival">tweet at us</a> and we'll see what we can do.</p>
-      <p>Attendees then sleepover in the venue before a second day of conference acitivites, before the final repsentations of projects which judges find the most interesting.</p>
+      <p>It starts with a science fair and then transitions into a conference until the evening when we will have multiple chill activities to choose from - movies, games, and more. If you have specific activities you'd like to see - <a href="https://twitter.com/BASHFestival">tweet at us</a> and we'll see what we can do.</p>
+      <p>Attendees then sleepover in the venue before a second day of conference activities, at the end of which our panel of judges will award prizes for the most innovative and creative projects.</p>
       <p>The weekend is free to attend thanks to our event sponsors, and it's expected that all attendees from the week will also attend the weekend. If cost is a barrier to this, take a look at information around our <n-link to='/weekend/access'>Access Scheme</n-link>.</p>
       <p>We'll share more details around the weekend closer to the event. For now you can find our <n-link to='/weekend/venue'>venue details</n-link> and <n-link to='/weekend/schedule'>weekend schedule</n-link>.</p>
     </div>
