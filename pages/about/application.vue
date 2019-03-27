6597095d503178c034ad43d9a524ca2a17be28f7
Kevin Lewis <kvn@lws.io>
2019-03-27 13:21:04 +0000

    application process updates

--- pages/about/application.vue
+++ pages/about/application.vue
@@ -6,8 +6,8 @@
       <p>We have decided to use an application-based ticketing system for BASH Festival of Code 2019. This is for a number of reasons:</p>
       <ul>
         <li>We want to provide a big and fair application window - making sure everyone who wants to gets a chance to apply, instead of those who happen to be quickest at getting tickets.</li>
-        <li>We want to ensure all Regional Hubs, and therefore our event as a whole, is full of induviduals from a diverse set of backgrounds and circumstances. Having applications helps us avoid scenarios where a hub lacks diversity.</li>
-        <li>In the case that young people are at the very beginning of their technical journey and can't code, we can signpost them to initatives which are more suitable for them.</li>
+        <li>We want to ensure all Regional Hubs, and therefore our event as a whole, is full of individuals from a diverse set of backgrounds and circumstances. Having applications helps us avoid scenarios where a hub lacks diversity.</li>
+        <li>In the case that young people are at the very beginning of their technical journey and can't code, we can signpost them to initiatives which are more suitable for them.</li>
       </ul>
       <p>Our applications in no way exist to pick the 'best' or 'most skilled' young people. Just a basic working knowledge of a written programming or markup language is all we require.</p>
       <h2>Initial Applications - Deadline May 15th 2019</h2>
@@ -17,9 +17,9 @@
       <p>Both parts must be filled out at the same time. Some questions help us later <n-link to='/about/success'>assess the success of our event</n-link> when put together with a post-event questionnaire.</p>
       <p><b>We'll send out acceptances to applicants and their parents or guardians on May 20th 2019. </b></p>
       <h2>Round 1 Acceptances - Deadline June 3rd 2019</h2>
-      <p>You'll then have 2 weeks to confirm your space at the event, which we ask you to only do if you are certain that you can attend. Both parents and attendees will need to accept, and this will be the point we collect formal permission documents from parents or guardians.</p>
+      <p>You'll then have 2 weeks to confirm your space at the event, which we ask you to only do if you are certain that you can attend. Both the attendees and their parent or guardian will need to accept, and this will be the point at which we collect formal permission documents from the adult supporting the application.</p>
       <h2>Round 2 Acceptances - Deadline June 11th 2019</h2>
-      <p>We expect some applicants won't be able to accept their invitations to BASH Festival of Code. On June 4th 2019 we will send out a second wave of invites which must be accepted by June 11th 2019.</p>
+      <p>We understand that some applicants may not be able to accept their 1st round places to BASH Festival of Code. On June 4th 2019 we will send out a second wave of invites which must be accepted by June 11th 2019.</p>
     </div>
   </div>
 </template>
