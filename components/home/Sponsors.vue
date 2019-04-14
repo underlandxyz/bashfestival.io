c9ad3b559729d7cc9d7b50f7b925367b94026876
Kevin Lewis <kvn@lws.io>
2019-04-14 17:10:54 +0100

    add puntofisso

--- components/home/Sponsors.vue
+++ components/home/Sponsors.vue
@@ -1,37 +1,41 @@
 <template>
   <div id='sponsors' class='w'>
-    <a href="https://wildrocket.io" target="_blank">
-      <img src="~/assets/img/sponsors/wildrocket.png" alt="Wild Rocket Dev Studio">
-    </a>
-    <a href="https://createk.io" target="_blank">
-      <img src="~/assets/img/sponsors/createk.png" alt="Createk">
-    </a>
-    <a href="https://www.rolls-royce.com/" target="_blank">
-      <img src="~/assets/img/sponsors/rollsroyce.png" alt="Rolls-Royce">
-    </a>
-    <a href="https://balsamiq.com" target="_blank">
-      <img src="~/assets/img/sponsors/balsamiq.png" alt="Balsamiq">
-    </a>
-    <a href="https://www.digitalocean.com" target="_blank">
-      <img src="~/assets/img/sponsors/digitalocean.png" alt="DigitalOcean">
-    </a>
-    <a href="https://www.ukgovcamp.com/" target="_blank">
-      <img src="~/assets/img/sponsors/ukgovcamp.png" alt="UKGovCamp Grants">
-    </a>
-    <a href="http://front-line-tech.com/" target="_blank">
-      <img src="~/assets/img/sponsors/frontlinetech.png" alt="Front-Line Technology">
-    </a>
-    <a href="https://www.barclays.co.uk/" target="_blank">
-      <img src="~/assets/img/sponsors/barclays.png" alt="Barclays">
-    </a>
+    <div class="top">
+      <a href="https://wildrocket.io" target="_blank">
+        <img src="~/assets/img/sponsors/wildrocket.png" alt="Wild Rocket Dev Studio">
+      </a>
+      <a href="https://createk.io" target="_blank">
+        <img src="~/assets/img/sponsors/createk.png" alt="Createk">
+      </a>
+      <a href="https://www.rolls-royce.com/" target="_blank">
+        <img src="~/assets/img/sponsors/rollsroyce.png" alt="Rolls-Royce">
+      </a>
+      <a href="https://balsamiq.com" target="_blank">
+        <img src="~/assets/img/sponsors/balsamiq.png" alt="Balsamiq">
+      </a>
+    </div>
+    <div class="bottom">
+      <a href="https://www.digitalocean.com" target="_blank">
+        <img src="~/assets/img/sponsors/digitalocean.png" alt="DigitalOcean">
+      </a>
+      <a href="https://www.ukgovcamp.com/" target="_blank">
+        <img src="~/assets/img/sponsors/ukgovcamp.png" alt="UKGovCamp Grants">
+      </a>
+      <a href="http://front-line-tech.com/" target="_blank">
+        <img src="~/assets/img/sponsors/frontlinetech.png" alt="Front-Line Technology">
+      </a>
+      <a href="https://www.barclays.co.uk/" target="_blank">
+        <img src="~/assets/img/sponsors/barclays.png" alt="Barclays">
+      </a>
+      <a href="http://puntofisso.net/" target="_blank">
+        <img src="~/assets/img/sponsors/puntofisso.png" alt="In Other News by @puntofisso">
+      </a>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 #sponsors {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 3em;
   margin-bottom: 3em;
   a {
     display: block;
@@ -39,8 +43,22 @@
       width: 100%;
     }
   }
+  .top {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 3em;
+    margin-bottom: 2em;
+  }
+  .bottom {
+    display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 3em;
+  }
   @media screen and (max-width: 700px) {
-    grid-template-columns: repeat(2, 1fr);
+    .top, .bottom {
+
+      grid-template-columns: repeat(2, 1fr);
+    }
   }
 }
 </style>
