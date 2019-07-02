af020cd2c13ee9d9beb3333c0a1087640bd439da
Kevin Lewis <kvn@lws.io>
2019-07-02 13:53:47 +0100

    Site survey and hub leader resource page

---
+++ pages/week/hubs/resources.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <SubHeaderWeek />
+    <div class='w content'>
+      <h1 id="title">Training</h1>
+      <p>Here is a list of links related to BASH Festival of Code Hub Leader Training, and all supplementary forms/information.</p>
+      <ul>
+        <li><a href='https://docs.google.com/presentation/d/1GOcy1DVu9mklak2w7CAeb9RfKgewPVuCXsZe3JDpw7o/edit?usp=sharing' target="_blank">Training presentation</a></li>
+        <li><a href='https://www.raspberrypi.org/safeguarding/e-learning-module' target="_blank">Raspberry Pi e-learning module for safeguarding</a></li>
+        <li><a href='https://bashfestival.io/dbs' target="_blank">DBS Check Form</a></li>
+        <li><a href='https://airtable.com/shrH0zoWw5clmBkrP' target="_blank">Hub Site Survey</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import SubHeaderWeek from '@/components/nav/subnavs/SubHeaderWeek'
+export default {
+  components: { SubHeaderWeek },
+  head () {
+    return { title: `Training - ${this.$store.state.title}` }
+  },
+}
+</script>
+
+<style scoped>
+  iframe {
+    margin-bottom: 4em;
+  }
+</style>
