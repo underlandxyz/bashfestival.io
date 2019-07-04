5ade8264cbcefba796d93d33df47d47c040da029
Kevin Lewis <kvn@lws.io>
2019-07-05 00:01:39 +0100

    Added privacy of person

--- pages/policies/privacy.vue
+++ pages/policies/privacy.vue
@@ -29,11 +29,33 @@
       </ol>
       <h2>Something feel out of place?</h2>
       <p>If you believe at any point that we are not following this privacy policy as stated, please contact us immediately via email (<a href='mailto:bash@underland.xyz'>bash@underland.xyz</a>), or via telephone (+44 7492 886145).<p>
+      <h2>Privacy of Person Policy</h2>
+      <p>These guidelines govern how participants are able to interact with their rights to privacy of their person when physically attending any or all parts of the BASH Festival of Code.</p>
+      <h3>Applicability & Scope</h3>
+      <p>This policy affects all participants at any level of responsibility or oversight, and is intended for consideration at all facing interactions in sites used for BASH Festival activities between parties with regard to the core dates of 5th to 11th August 2019.</p>
+      <p>The remit in all cases applies only to the participant directly affected.</p>
+      <h3>Your Rights</h3>
+      <ul>
+        <li>
+          <p><b>Personal space</b></p>
+          <p>You are the arbiter of what constitutes as comfortable and sufficient personal space. If you feel that you are not being afforded the reasonable minimum of this by another party then you have the right to politely and firmly express this to the party who is encroaching upon your immediate space and causing the discomfort.</p>
+        </li>
+        <li>
+          <p><b>How you respond to questions</b></p>
+          <p>You have the right to choose what you will and will not respond to when receiving communication that invites response. If you do not feel comfortable with responding to the manner in which you are addressed, or the subject of the communication, then you are within your rights to disregard it in whole or politely refrain in part from supplying the information about yourself when personally communicated with.</p>
+        </li>
+        <li>
+          <p><b>Seclusion</b></p>
+          <p>You have the right to request that you be afforded access to a private space where you can be alone if you so require. This will be accommodated where practical unless it conflicts with the circumstances around which other individuals are similarly accommodated, or in any other way detracts from the concerns of the larger group. </p>
+        </li>
+      </ul>
+      <p>If you have any questions about these criteria and how they make affect your involvement in the BASH Festival of Code then <n-link to='/contact'>please get in touch</n-link>.</p>
       <h2>Changes to policy</h2>
       <p>This policy might change over time, which we reserve the right to do without notice. If we do make edits, you'll find them listed below.</p>
       <div class="log">
         <h2>Revisions</h2>
         <ul>
+          <li>04 July 2019: Added Privacy of Person Policy</li>
           <li>22 March 2019: Initial version written</li>
         </ul>
       </div>
