44b7539a2ca874d71a0a11550a05c51d02ac6147
Kevin Lewis <kvn@lws.io>
2019-08-02 16:41:17 +0100

    Hub Leader crib sheet

---
+++ pages/guides/leaders.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <SubHeaderGuides />
+    <div class='w content'>
+      <h1 id="title">Hub Leader Crib Sheet</h1>
+      <ul>
+        <li><a href='https://docs.google.com/presentation/d/1GOcy1DVu9mklak2w7CAeb9RfKgewPVuCXsZe3JDpw7o/edit?usp=sharing' target="_blank">Training presentation</a></li>
+        <li><a href='https://www.raspberrypi.org/safeguarding/e-learning-module' target="_blank">Raspberry Pi e-learning module for safeguarding</a></li>
+      </ul>
+      <ul>
+        <li><n-link to='/crib'>Attendee crib sheet</n-link></li>
+        <li><n-link to='/guides/videos'>Daily videos (updated late each night)</n-link></li>
+        <li><a href="https://bashfestival.io/reporting">End of day reporting</a></li>
+        <li><n-link to='/policies/conduct'>Code of Conduct</n-link></li>
+        <li><n-link to='/waiver'>Waiver for under 16s to travel alone</n-link></li>
+        <li>
+          <p><b>If attendee is under 16, arrives without parent, and doesn't have a parental waiver</b></p>
+          <p>We have a duty of care. Allow the attendee to take part but you must get hold of their parent/guardian before releasing them and have them fill in the waiver. Please tell us.</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import SubHeaderGuides from '@/components/nav/subnavs/SubHeaderGuides'
+export default {
+  components: { SubHeaderGuides },
+  head () {
+    return { title: `Hub Leader Crib Sheet - ${this.$store.state.title}` }
+  },
+}
+</script>
+
+<style scoped>
+  ul {
+    margin-bottom: 2em;
+  }
+  p {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+</style>
